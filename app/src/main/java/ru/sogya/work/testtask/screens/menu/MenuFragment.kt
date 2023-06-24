3503140ee105b1af0d4f8c25ad0ecd4e719c9f01
SogYa <Ghost.Spartan36@gmail.com>
2023-06-24 16:50:53 +0300

    added live data funcs

--- app/src/main/java/ru/sogya/work/testtask/screens/menu/MenuFragment.kt
+++ app/src/main/java/ru/sogya/work/testtask/screens/menu/MenuFragment.kt
@@ -7,12 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 import ru.sogya.work.testtask.R
 import ru.sogya.work.testtask.databinding.FragmentMenuBinding
 import ru.sogya.work.testtask.screens.menu.adapters.CategoryAdapter
 import ru.sogya.work.testtask.screens.menu.adapters.HintsAdapter
 import ru.sogya.work.testtask.screens.menu.adapters.MenuAdapter
-
+@AndroidEntryPoint
 class MenuFragment : Fragment(R.layout.fragment_menu) {
     private val vm: MenuVM by viewModels()
     private lateinit var binding: FragmentMenuBinding
@@ -27,6 +28,17 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
     ): View {
         binding = FragmentMenuBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.getCategoryLiveData().observe(viewLifecycleOwner){
+           categoryAdapter.updateCategoryList(it)
+        }
+        vm.getMenuLiveData().observe(viewLifecycleOwner){
+            menuAdapter.updateMenuList(it)
+            binding.loadingLayout.visibility = View.GONE
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/ru/sogya/work/testtask/screens/menu/MenuVM.kt
+++ app/src/main/java/ru/sogya/work/testtask/screens/menu/MenuVM.kt
@@ -1,7 +1,17 @@
 package ru.sogya.work.testtask.screens.menu
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.sogya.work.testtask.domain.usecase.GetCategoryUseCase
+import ru.sogya.work.testtask.domain.usecase.GetMenuUseCase
+import javax.inject.Inject
 
-class MenuVM : ViewModel() {
+@HiltViewModel
+class MenuVM @Inject constructor(
+    private val getCategoryUseCase: GetCategoryUseCase,
+    private val getMenuUseCase: GetMenuUseCase
+): ViewModel() {
+    fun getMenuLiveData() = getMenuUseCase()
 
+    fun getCategoryLiveData() = getCategoryUseCase()
 }
